41aa3d3e0428a97e022d52e9a81c930336b53970
vishal9884 <111237839+vishal9884@users.noreply.github.com>
2023-03-26 19:18:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 provider "aws" {
   # Configuration options
     region =  "us-east-1"
-    access_key = "ASIA6FZEJH5TOXQY5YWY"
-    secret_key = "3Islu1ds2P71AZnCfqte/zJ59ILqlKkojlRBG5ne"
-    token = "FwoGZXIvYXdzEDIaDD78r1+YDfqfcFIglyK1AR7bRWh799eRHZkZ1P6BzMldS9VFfuHHWYQl6KqOXZOzPxRHqPYDBENZ+kOdy7PydCPUGbLTZtfy8rnLNj8YdCq1viw5jMzsi5aLCAzWm7vkKiTozS8dbwFfOYZxk96A0KCLNkOotLqEMtaf/pNSqm1p7gA472swiVj/nMj1XuDzBNJ1IwFLGKRpE5wLxg7YAIUS9goGemUe0PRHMoKeJam9djECU8zqZn+xkRSHEccdwV5djLwo6t/6oAYyLcZRwFLn4EbLWfmBcm0SfVrlknD23T3GZDS8TENqwqy6+L5/nmfZzCikO15fqg=="
+    access_key = "ASIA6FZEJH5TLZ5C75OQ"
+    secret_key = "4atRLoTljlNBIRcURV/pccnzWOzqCpNxPE1qWaIQ"
+    token = "FwoGZXIvYXdzEE8aDL6g/RAVobLueSlCHyK1AbMGTYa11udq82XeJIWnJykkshKusq+6KFQtxIE2CnHXO1H7Zky3uHQ2FSF2iXMVrcKACkBOCjSYydcI/eDZ5PAtEfyqVsU65slWlsWsUP5QcHQ1i4OXLZUyDec+B6ZVzK9uhkKKRlParg1YdktFwatAmTb9HV41Uz9pQYbsbKCdSWlefDTOQ0kOzIJiBLuFpt/ELo6MiblGtAmTf6vIRwJBGRlLbCKkfE74HiPvHiiR+BMDmuMowYqBoQYyLQ9mISKRLMsypPjX5V0mHppU1uVmtvbseLhVwmYDV9xWZoY1UhE22OTdWfSVIg=="
 }
